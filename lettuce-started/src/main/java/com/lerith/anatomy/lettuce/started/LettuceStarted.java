0e17f42df784491a88a10c6af9d5d4a59e5b9e26
pao _ding <pao_ding@outlook.com>
2020-04-23 22:43:54 +0800

    Lettuce started

---
+++ lettuce-started/src/main/java/com/lerith/anatomy/lettuce/started/LettuceStarted.java
@@ -0,0 +1,19 @@
+package com.lerith.anatomy.lettuce.started;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+public class LettuceStarted {
+    public static void main(String[] args) {
+        RedisClient redisClient = RedisClient.create("redis://localhost:6379/0");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> syncCommands = connection.sync();
+
+        syncCommands.set("key", "Hello, Redis!");
+        String value = syncCommands.get("key");
+        System.out.println("value = " + value);
+        connection.close();
+        redisClient.shutdown();
+    }
+}
